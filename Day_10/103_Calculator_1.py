983909ca6ee09f154f60cc2ce2e488c705402463
ArneLucas <arnelucas@gmail.com>
2022-02-14 16:10:33 +0100

    Homework day ??

---
+++ Day_10/103_Calculator_1.py
@@ -0,0 +1,49 @@
+import os
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+from Calculator_art import logo
+
+def add(n1, n2):
+    return n1 + n2
+
+def substract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": divide
+    }
+
+print(logo)
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number? "))
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number? "))
+
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input("Continue? 'y' or 'n': ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            clearConsole()
+            calculator()
+
+calculator()
---
+++ Day_10/functions.md
@@ -0,0 +1,27 @@
+# Simple function:
+
+def my_simple_function():
+    # do this,
+    # then this,
+    # and this
+
+my_simple_function()
+
+
+# Function with input:
+
+def my_func_with_input(something):
+    # do this with (something)
+    # then this
+    # and this
+
+my_simple_function(something)
+
+
+# Function with output:
+
+def my_func_with_output():
+    result = 3 * 2
+    return result
+
+# output = my_func_with_output() # == result (6)
