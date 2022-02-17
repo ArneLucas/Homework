4fcc511b10ddb53359955786bd48660dc8ef4fbb
ArneLucas <arnelucas@gmail.com>
2022-02-17 12:44:09 +0100

    .

--- Day_10/103_Calculator_1.py
+++ Day_10/103_Calculator_1.py
@@ -1,27 +1,37 @@
-import os
-clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 from Calculator_art import logo
+import os
+
+
+def clearConsole(): return os.system(
+    'cls' if os.name in ('nt', 'dos') else 'clear')
+
 
 def add(n1, n2):
     return n1 + n2
 
+
 def substract(n1, n2):
     return n1 - n2
+
 
 def multiply(n1, n2):
     return n1 * n2
 
+
 def divide(n1, n2):
     return n1 / n2
+
 
 operations = {
     "+": add,
     "-": substract,
     "*": multiply,
     "/": divide
-    }
+}
 
 print(logo)
+
+
 def calculator():
     print(logo)
     num1 = float(input("What's the first number? "))
@@ -45,5 +55,6 @@ def calculator():
             should_continue = False
             clearConsole()
             calculator()
+
 
 calculator()
--- Day_10/99_Functions_with_Outpots.py
+++
@@ -1,9 +0,0 @@
-def format_name(f_name, l_name):
-    title_case_name = f_name.title() + " " + l_name.title()
-    return title_case_name
-
-f_name = input("What's your first name? ")
-l_name = input("What's your last name? ")
-formatted_name = format_name(f_name, l_name)
-
-print(formatted_name)
